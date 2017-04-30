04a1896789f4dd09d5b5d03201c053882f4584b0
Your name <you@example.com>
2017-04-30 19:50:52 +0300

    Changing button color

--- Main.py
+++ Main.py
@@ -2,7 +2,7 @@ import sys
 
 from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QCheckBox, QLabel, QGridLayout, QTableWidget,
                              QTableWidgetItem)
-from PyQt5.QtGui import QIcon
+from PyQt5.QtGui import QIcon, QColor
 
 
 class Account:
@@ -40,31 +40,41 @@ class AccountsTable(QTableWidget):
 
         self.setRowCount(10)
         self.setColumnCount(6)
-        self.setItem(0, 0, QTableWidgetItem("Id"))
-        self.setItem(0, 1, QTableWidgetItem("Login"))
-        self.setItem(0, 2, QTableWidgetItem("Password"))
-        self.setItem(0, 3, QTableWidgetItem("Status"))
-        self.setItem(0, 4, QTableWidgetItem("Settings"))
-        self.setItem(0, 5, QTableWidgetItem("On/Off"))
+        self.setItem(0, 0, QTableWidgetItem("Номер"))
+        self.setItem(0, 1, QTableWidgetItem("Логин"))
+        self.setItem(0, 2, QTableWidgetItem("Пароль"))
+        self.setItem(0, 3, QTableWidgetItem("Статус"))
+        self.setItem(0, 4, QTableWidgetItem("Настройки"))
+        self.setItem(0, 5, QTableWidgetItem("Вкл/Выкл"))
 
         for i in range(9):
-            button = QPushButton(f'Settings {i+1}')
+            button = QPushButton(f'Настройки {i+1}')
             button.clicked.connect(self.pressedSettings)
-            self.setCellWidget(i + 1, 4, button)
+            self.setCellWidget(i+1, 4, button)
 
         for i in range(9):
-            button = QPushButton(f'On/Off {i+1}')
+            button = QPushButton(f'Вкл/Выкл {i+1}')
             button.setCheckable(True)
             button.clicked.connect(self.pressedButtonOnOff)
-            self.setCellWidget(i + 1, 5, button)
+            self.setCellWidget( i+1, 5, button)
 
     def pressedSettings(self, event):
-        sender = self.sender().text()[-1]
-        print(sender)
+        sender = int(self.sender().text()[-1])
+        try:
+            current_account_id = self.item(sender, 0).text()
+            current_account_login = self.item(sender, 1).text()
+            current_account_password = self.item(sender, 2).text()
+            current_account_status = self.item(sender, 3).text()
+            if current_account_id == '' or current_account_login == '' or current_account_password == ''\
+                                                                        or current_account_status == '':
+                raise Exception
+            print(current_account_id, current_account_login, current_account_password, current_account_status)
+            self.cellWidget(sender, 4).setStyleSheet("background-color: #E1E1E1")
+        except:
+            self.cellWidget(sender, 4).setStyleSheet("background-color: red")
 
     def pressedButtonOnOff(self, event):
         sender = self.sender().text()[-1]
-
 
     def dragEnterEvent(self, e):
         if e.mimeData().hasFormat('text/plain'):
@@ -82,6 +92,7 @@ class MainWindow(QWidget):
         super().__init__()
         self.grid = MainGrid()
         self.table = AccountsTable()
+        self.addButton = QPushButton()
 
         self.addWidgets()
         self.initUI()
@@ -94,7 +105,9 @@ class MainWindow(QWidget):
         self.show()
 
     def addWidgets(self):
-        self.grid.addWidget(self.table, 0, 0)
+        self.grid.addWidget(self.table, 0, 0, 0, 4)
+        self.grid.addWidget(self.addButton, 0, 1, 0, 0)
+        self.addButton.show()
 
 
 def main():
